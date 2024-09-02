c4366a2343dbf3857da250c0fb3a1bda7bebe802
Madhusudan Kumar <madhusudan.kumar@tmotions.com>
2024-09-02 16:01:21 +0530

    menu

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+/* 
+    -------------------------- Mobile Menu ----------------------
+*/
+const menuIcon = document.querySelector('.menuIcon img');
+const nav = document.querySelector('nav');
+
+nav.style.display = 'none'; //initially setting the display
+
+menuIcon.addEventListener('click', () => {
+    if(nav.style.display === 'none') {
+        nav.style.display = 'block';
+        menuIcon.setAttribute('src', 'images/menu-cross-button.png');
+    } else {
+        nav.style.display = 'none';
+        menuIcon.setAttribute('src', 'images/menu-button.png');
+    }
+});
+
+/* 
+    -------------------------- Pop-up question 10% scroll ----------------------
+*/
