7ff540a33f73dbc9e46e0fb21c76afb0f1a1461b
Madhusudan Kumar <madhusudan.kumar@tmotions.com>
2024-09-05 18:21:24 +0530

    banner image size issue solved

--- scripts/index.js
+++ scripts/index.js
@@ -16,30 +16,6 @@ menuIcon.addEventListener('click', () => {
     }
 });
 
-/* 
-    -------------------------- Banner Background image ----------------------
-*/
-const heroImage = document.querySelector('.heroImage');
-
-function setBackground() {
-    const vpWidth = window.innerWidth;
-
-    if (vpWidth >= 1440) {
-        heroImage.setAttribute('src', 'images/desktop-banner.png');
-    } else if (vpWidth >= 1024) {
-        heroImage.setAttribute('src', 'images/laptop-banner.png');
-    } else if (vpWidth >= 768) {
-        heroImage.setAttribute('src', 'images/tab-banner.png');
-    } else {
-        heroImage.setAttribute('src', 'images/mobile-banner.png');
-    }
-}
-
-// Set the background image on initial load
-setBackground();
-
-// Update the background image on window resize
-window.addEventListener('resize', setBackground);
 
 /* 
     -------------------------- Pop-up question 10% scroll ----------------------
